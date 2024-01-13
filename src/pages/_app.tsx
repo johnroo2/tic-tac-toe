5fad01dd15431117ead7f8b6fa6c6471b51cc8d0
John Liu <johnliu@v1041-wn-rt-b-199-173.campus-dynamic.uwaterloo.ca>
2024-01-13 14:32:54 -0500

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'aos/dist/aos.css';
+import AOS from 'aos'
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    AOS.init()
+  }, [])
+
+  return (
+    <div className="relative w-screen h-screen overflow-hidden">
+      <div className="absolute inset-0 dots pointer-events-none z-[-1]"/>
+      <Component {...pageProps}/>
+
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,86 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState} from "react"
 
-const inter = Inter({ subsets: ['latin'] })
+function WinnerModal({state, reset}:any){
+  return(
+    <div className="absolute inset-0 flex items-center justify-center bg-black/[0.2]">
+      <div className="self-center w-[250px] h-[250px] 
+      flex flex-col items-center justify-center bg-white rounded
+      text-3xl font-bold gap-2" data-aos="fade-up">
+        Winner: {state === 1 ? <span className="text-rose-800 text-[2.5rem]">&times;</span>:
+        <span className="text-indigo-800">&#x25CB;</span>}
+        <button className="mt-4 text-black hover:text-rose-700 underline hover:underline
+        transition rounded text-[0.6em] font-medium mb-[-0.5rem]"
+        onClick={reset}>
+          Reset Game
+        </button>
+      </div>
+    </div>
+  )
+}
 
 export default function Home() {
+  const [turn, setTurn] = useState<number>(1)
+  const [board, setBoard] = useState<number[]>([0, 0, 0, 0, 0, 0, 0, 0, 0])
+  const [modalState, setModalState] = useState<number>(0)
+
+  const reset = () => {
+    setBoard([0, 0, 0, 0, 0, 0, 0, 0, 0])
+    setTurn(1)
+    setModalState(0)
+  }
+
+  const checkState = (board:number[], turn:number) => {
+    for (let i = 0; i < 3; i++) {
+      if (Math.abs(board[i * 3] + board[i * 3 + 1] + board[i * 3 + 2]) === 3 ||
+        Math.abs(board[i] + board[i + 3] + board[i + 6]) === 3) {
+        setModalState(turn);
+      }
+    }
+  
+    if (Math.abs(board[0] + board[4] + board[8]) === 3 ||
+        Math.abs(board[2] + board[4] + board[6]) === 3) {
+      setModalState(turn);
+    }
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="relative flex flex-col items-center justify-center h-screen gap-4" data-aos="fade-up">
+        <div className="text-black font-bold text-5xl text-center">
+          Tic-Tac-Toe
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="text-black font-medium text-xl text-center h-[2rem] flex flex-row items-center gap-[0.35em]">
+          {turn === 1 ? <span className="text-rose-800 text-[1.65rem] mb-1">&times;</span>:
+          <span className="text-indigo-800">&#x25CB;</span>} to move
+        </div>
+        <div className="flex flex-row flex-wrap w-[360px]">
+          {board.map((square, index) => (
+            <button className="w-[120px] h-[120px] border-black border-2 m-[-1px] 
+            bg-white/[0.3] hover:bg-white/[0.9] flex items-center justify-center
+            font-light transition" key={index}
+            onClick={() => {
+              setBoard((prev) => {
+                const clone = [...prev]
+                if(clone[index] === 0){
+                  clone[index] = turn
+                  checkState(clone, turn)
+                  setTurn(prev => -prev)
+                }
+                return clone
+              })
+            }}>
+              {square === -1 ? <span className="text-indigo-800 text-[65px]">&#x25CB;</span> : 
+              square === 1 ? <span className="text-rose-800 mb-2 text-[80px]">&times;</span> : <></>}
+            </button>
+          ))}
+        </div>
+        <button className="mt-2 text-white bg-rose-700 hover:bg-rose-500 py-1 
+        px-10 transition rounded text-md font-medium"
+        onClick={reset}>
+          New Game
+        </button>
+      </main>
+      {modalState !== 0 && <WinnerModal state={modalState} reset={reset}/>}
+    </>
   )
 }
